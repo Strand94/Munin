602a57d3225f019b4907e3ac96cc702807aafca4
Andreas Strand <anstra@stud.ntnu.no>
2017-02-16 20:47:36 +0100

    Adding school to database

--- apps/staticpages/models.py
+++ apps/staticpages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+#Model for school, to keep track of schools and their info.
+class School(models.Model):
+    name = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='schools', default='placeholder-school-logo.jpg')
+    telephone = models.CharField(max_length=12, blank=True)
+    address = models.CharField(max_length=70, blank=True)
+    webpage = models.CharField(max_length=150, blank=True)
+
+    def __str__(self):
+        return self.name
