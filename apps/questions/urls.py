073f95f5841b44add4cef53266c758d0ec2d72aa
TrulsElg <trulsel@stud.ntnu.no>
2017-03-22 15:52:37 +0100

    Added functionality so that you can enter a question and that question text will be saved in the database

--- apps/questions/urls.py
+++ apps/questions/urls.py
@@ -6,4 +6,5 @@ from apps.questions.views import *
 urlpatterns = [
     url(r'^$', Questions, name="questions"),
     url(r'^ask_new_question$', AskNewQuestion, name="ask_new_question"),
+    url(r'^your_question$', YouAskedAQuestion, name="your_question"),
 ]
--- apps/questions/views.py
+++ apps/questions/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from apps.questions.models import Question
 
 # Create your views here.
@@ -7,4 +8,22 @@ def Questions (request):
     return render(request,"questions/questions.html")
 
 def AskNewQuestion (request):
-    return render(request,"questions/ask_new.html")
+    return render(request, "questions/ask_new.html")
+
+def YouAskedAQuestion (request):
+        if request.method == 'POST':
+
+            user = request.user
+            question_text = request.POST.get('question_box', None)
+
+            if question_text == '':
+                return HttpResponseRedirect("/questions/ask_new_question")
+
+            question = Question()
+            question.question = question_text
+            question.asked_question = user
+            question.save()
+
+            return render(request, "questions/you_asked_a_question.html")
+        else:
+            return render(request, "questions/ask_new.html")
